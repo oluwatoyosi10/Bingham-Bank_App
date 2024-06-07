d96730c1ed5466cfc12d7a0b2e3e8643e084e5bf
oluwatoyosi10 <149247316+oluwatoyosi10@users.noreply.github.com>
2024-06-07 04:57:20 -0700

    Delete test testing.py

--- test testing.py
+++
@@ -1,8 +0,0 @@
-class user:
-    def register(self, name, phone, email, password):
-        return name
-    def login(self, email, password):
-        if email=="testmail.com and password" == "ABC":
-
-    user = user()
-    print(user.register("james", "08092929292", "james@gmail.com", "james12345"))
