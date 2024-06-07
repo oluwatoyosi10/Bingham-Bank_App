f7345c2d20a51111c56696067ac4513510893fe2
oluwatoyosi10 <149247316+oluwatoyosi10@users.noreply.github.com>
2024-06-07 04:59:47 -0700

    Delete Registration2.py

--- Registration2.py
+++
@@ -1,5 +0,0 @@
-person = input("please enter your name:")
-number = input("please enter your email address:")
-password = input("please enter your password:")
-
-def login(person, number, password):
